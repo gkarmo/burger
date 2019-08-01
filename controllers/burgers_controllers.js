86d264a056358cf0ed57bf01b50cf716e183bbd8
Gayah Karmo <gayahkarmo@gmail.com>
2019-07-31 23:56:31 -0400

    1st attempt burgers showing

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -4,5 +4,22 @@ var router = express.Router();
 
 var burgers = require("../models/burgers.js");
 
+router.get("/", function(req, res) {
+    burgers.selectAll(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+
+  router.post("/api/burgers", function(req, res) {
+    burgers.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+
 
 module.exports = router;
